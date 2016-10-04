e2a24feaaf51f00010a9790865a7220d04e7baa2
Paul Andrei R. Caoile <andreicaoile@yahoo.com>
2016-10-05 01:52:00 +0800

    Add files via upload

--- MTM.cpp
+++ MTM.cpp
@@ -3,28 +3,31 @@
 #include<iomanip>
 #include<cmath>
 #include<string.h>
+#include<windows.h>
+#include<fstream>
 using namespace std;
 int ans;
+char uname[20];
+char format[4]={'.','t','x','t'};
+void checker(); //function for the user
+void bmenu(); //function for the menu
+void title(); //function for the title
+void withdraw(); //function for withdraw
+void deposit(); //function for deposit
+void balance(); //function for balance
+void transfer(); //function for transfer amount
+void finalwrite(); //Save new Balance
 
-void checker();
-void bmenu();
-void title();
-void withdraw();
-void deposit();
-void balance();
-void transfer();
-
-struct Profile{
+struct Profile{ //file handling
 	struct{
-	char fname; // First Name
-	char lname;// Last   "
+	char uname; //user name
 	}Name;
 	struct {
-	char cnum[11];
 	char pin[4];
 	}	Cdetails;
 	float Balance;
 };
+
 
 
 
@@ -34,15 +37,17 @@ void newLine();
 
 main(){
 	cout<<fixed<<setprecision(2);
+	checker(); //this will directly go to function checker
 	do{
-	system("cls");
-	bmenu();
-	system("cls");
+	system("cls"); //this will prompt the user
+	bmenu(); //directly go to function menu
+	system("cls"); //this will prompt the user
 	cout<<endl;
 	cout<<"Another Transaction?\nPress [1] to make another transaction\nPress [2] to exit\n";
-	cin>>ans;
+	cin>>ans; // this will make another transaction
 	}while(ans==1);
-	system("pause");
+	finalwrite();
+	system("pause"); //this will prompt the user
 	return 0;
 }
 
@@ -56,73 +61,141 @@ main(){
 
 
 
+void checker(){ //code for the user
+	int i,b;
+	
+	char inpin[4];
+	char filename[40];
+	
+	int leng(0),u;
+	title();
+	cout<<"\n\n\n";
+	cout<<"Welcome"<<endl;
+	
+	cout<<"Enter User Name: ";
+	cin>>uname; //this will get every character with an specific size
+	
+	
+	
+	 ifstream info(strcat(uname,format));
+	 int *bal;
+	 if(info.is_open()){
+	 	for(u=0;u<4;u++){
+	 	info>>prof.Cdetails.pin[u];
+	 	
+		 }
+		info>>prof.Balance;
+	 }
+	 else{
+	 	cout<<"Invalid Username / Username does not exist\n";
+	 	cin.ignore();
+	 	exit(0);
+	 }
+	 
+	 
+	cout<<"Enter Your Pin: "; 
+	for(i=0;i<4;i++){ //the output of the pin is asterisk
+		inpin[i]=getch();
+		cout<<"*";
+		Beep(700,300);
+	}
+	cout<<endl;
+		if(strcmp(prof.Cdetails.pin,inpin)==0){ //this will compare the inputed pin to another conformation pin 
+		cout<<"Pin Successfully Entered..."<<endl;
+		
+	}
+	else{
+		cout<<"Pin does not match..."<<endl;
+		cin.ignore();
+		exit(0);
+	}
+
+	
+	system("pause"); //this will prompt the user
+	
+}
 
 
-
-void deposit(){
+void deposit(){ //code for deposit
 	float depo;
-	system("cls");
+	system("cls"); //this erase the previous output
 	title();
 	cout<<"Enter Amount to Deposit: ";
 	cin>>depo;
 	cout<<endl<<endl<<"Deposit Money"<<endl;
-	cin.ignore();
-	cout<<"Previous Balance: "<<prof.Balance<<endl;
+	cin.ignore(); //depositing money
+	cout<<"Previous Balance: "<<"P"<<prof.Balance<<endl;
 	prof.Balance+=depo;
-	cout<<"New Balance: "<<prof.Balance<<endl;
+	cout<<"New Balance: "<<"P"<<prof.Balance<<endl;
 	
 	
-	system("pause");
+	system("pause"); //this will prompt the user
 }
 
-void balance(){
-	system("cls");
+void balance(){ //code for balance account
+	system("cls"); //this erase the previous output
 	title();
-	cout<<prof.Cdetails.cnum
-	cout<<"Your Balance is: "<prof.Balance;
-	system("Pause");
+	cout<<"Your Balance is: "<<prof.Balance<<endl;
+	system("Pause"); //this will prompt the user
 }
 
-void transfer(){
-	float trans;
-	char text[20];
-	system("cls");
-	title();
-	cout<<"Enter Amount to Transfer: ";
+void transfer(){ //code for transfering money
+	float trans; 
+	char text[20]; 
+	system("cls"); //this will erase previous the output
+	title(); //this will display title
+	cout<<"Enter Amount to Transfer: "; 
 	cin>>trans;
+	cin.ignore(); 
 	cout<<"Enter Recipient: ";
-	cin.getline(text,19);
-	
+	cin.getline(text,19); //this will input the name of the user
 	
 	cout<<endl<<endl<<"Press any key for confirmation"<<endl;
-	cin.ignore();
-	cout<<"Previous Balance: "<<prof.Balance<<endl;
+	system("pause"); //this will prompt the user
+	if(trans<prof.Balance) //this will compare the transfered amount to your balance
+	{
+	cout<<"Previous Balance: "<<"P"<<prof.Balance<<endl;
 	prof.Balance-=trans;
-	cout<<"New Balance: "<<prof.Balance<<endl;
-	cout<<trans<<"Successfully transferred to: "<<text;
-	system("pause");
-	system("pause");
+	cout<<"New Balance: "<<"P"<<prof.Balance<<endl;
+	cout<<"P"<<trans<<" Successfully transferred to: "<<text<<endl;
+}
+else
+{
+	system("cls"); //this erase the previous output
+	cout << "Amount suceeded preceeding balance !" << endl; 
+}
+	system("pause"); //this will prompt the user
 }
 
 void withdraw(){
-	system("cls");
-	title();
+	system("cls"); //this erase the previous output
+	title(); //this will display title
 	float with;
 	cout<<"Enter Amount to Withdraw: ";
 	cin>>with;
-	cout<<endl<<endl<<"Press any key to get the money"<<endl;
-	cin.ignore();
-	cout<<"Previous Balance: "<<prof.Balance<<endl;
-	prof.Balance-=with;
-	cout<<"New Balance: "<<prof.Balance<<endl;
-	
+	if(with<prof.Balance) //this will compare the inputed withdraw to your balance
+	{
+	cout<<endl<<endl<<"To claim the money"<<endl;
 	system("pause");
+	for(int b=0;b<=5;b++){ //this will create beep sound
+		Beep(900,100);	
+	}
+	cout<<"Previous Balance: "<<"P"<<prof.Balance<<endl;
+	prof.Balance-=with;
+	cout<<"New Balance: "<<"P"<<prof.Balance<<endl;
+	}
+	else 
+	{
+		cout << "invalid amount !"<<endl;
+		
+		}	
+	system("pause"); //this will prompt the user
 	
 }
 
-void bmenu(){
+void bmenu(){ //code for menu
 	int choice;
-	title();
+	title(); //display title
 	cout<<endl<<endl;
 	cout<<"Select Transaction: "<<endl;
 	cout<<setw(12)<<"[1]:Withdraw Money" << setw(50)<<"[2]:Deposit Money\n";
@@ -131,36 +204,35 @@ void bmenu(){
 	cout<<setw(50)<<"********************************************************************************"<<endl;
 	cin>>choice;
 	
-		switch(choice){
+		switch(choice){ //this will determine the choice
 			case 1:{
-				withdraw();
+				withdraw(); //will go to withdraw function
 				break;
 			}
 			case 2:{
-				deposit();
+				deposit(); //will go to deposit function
 				break;
 			}
 			case 3:{
-				balance();
+				balance(); //will go to balance function
 				break;
 			}
 			case 4:{
-				transfer();
+				transfer(); //will go to transfer function
 				break;
 			}
-			case 5:{
-				system("cls");
-				cout<<"Thank you for using this machine...";
-				system("pause");
-				exit(0);
+			case 5:{ 
+				system("cls"); //this erase the previous output
+				cout<<"Thank you for using this machine...\n";
+				system("pause"); //this will prompt the user
+				exit(0); //will exit the program
+				finalwrite();
 				break;
 			}
-			default:{
-				system("cls");
-				cout<<"Invalid Choice";
-				system("pause");
-				system("cls");
-				return bmenu();
+			default:{ //for invalid
+				system("cls"); //this erase the previous output
+				cout<<"Invalid Choice\n";
+				system("cls"); //will exit the program
 				break;
 			}
 		}
@@ -168,9 +240,28 @@ void bmenu(){
 	
 
 
-void title(){
+void title(){ //will display title
 	cout<<setw(50)<<"********************************************************************************"<<endl;
 	cout<<setw(50)<<"SAFE DREAMS BANK"<<endl;
 	cout<<setw(57)<<"Make your dream in the reality"<<endl;
 	cout<<setw(50)<<"********************************************************************************"<<endl;
 }
+
+void finalwrite(){
+	int u;
+	ofstream update(uname);
+	for(u=0;u<4;u++){
+	 	update<<prof.Cdetails.pin[u]<<endl;
+	 	
+		 }
+		update<<prof.Balance;
+		 update.close();
+}
+/* 
+CATM (Card-less Automatic Teller? Machine)
+Account Storage (File Handling)***
+Menu Function(Checker fxn)
+Main Menu (bmenu fxn prioritize switch)
+In Choice Functions (dep, with, trans,)
+Pangwakas Afterspeech
+*/
